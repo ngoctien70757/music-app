e8c0196852b5f63bfd3f8df45c9f042875db4e3b
Ngoc Tien <ngoctien3532@gmail.com>
2023-03-05 13:25:57 +0700

    init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninFormComponent } from './modules/signin/components/signin-form/signin-form.component';
+import { SignupFormComponent } from './modules/signin/components/signup-form/signup-form.component';
+import { SigninComponent } from './modules/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //home layout
+  { path: '', redirectTo: 'SignIn', pathMatch: 'full' },
+  {
+    path: '',
+    component: SigninComponent,
+    children: [
+      { path: 'signin', component: SigninFormComponent },
+      { path: 'signup', component: SignupFormComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './modules/signin/signin.component';
+import { SigninFormComponent } from './modules/signin/components/signin-form/signin-form.component';
+import { SignupFormComponent } from './modules/signin/components/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SigninFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
